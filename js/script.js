86783944e6dcfbdf3d8ca7e2e159752492657ad8
hiblurryface <AlexandrKsenz@gmail.com>
2021-04-19 01:34:52 +0300

    master

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    $('.header__burger').click(function (event) {
+        $('.header__menu').toggleClass('active');
+    })
+})
+
+window.onscroll = function showHeader() {
+    var header = document.querySelector('.header');
+    if (window.pageYOffset > 1) {
+        header.classList.add('header_active');
+    } else {
+        header.classList.remove('header_active');
+    }
+}
