299a34871be6eac0c0fff257de8cce478606f1d6
hiblurryface <AlexandrKsenz@gmail.com>
2021-04-19 16:59:41 +0300

    master

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
     $('.header__burger').click(function (event) {
         $('.header__menu').toggleClass('active');
+        $('body').toggleClass('lock');
     })
 })
 
